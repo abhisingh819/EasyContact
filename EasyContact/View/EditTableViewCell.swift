bdece4177822a4a99d766c38b154edf9f4edbb63
abhinav <singhabhi1115@gmail.com>
2019-06-02 04:42:28 +0530

    add contact screen created and navigation added

---
+++ EasyContact/View/EditTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EditTableViewCell.swift
+//  EasyContact
+//
+//  Created by Abhinav Singh on 6/2/19.
+//  Copyright © 2019 Abhinav. All rights reserved.
+//
+
+import UIKit
+
+class EditTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ EasyContact/ViewModel/EditContactViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  EditContactViewModel.swift
+//  EasyContact
+//
+//  Created by Abhinav Singh on 6/2/19.
+//  Copyright © 2019 Abhinav. All rights reserved.
+//
+
+import Foundation
